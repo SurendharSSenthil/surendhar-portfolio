509d66f411aa1e6eed0eb7589120871eee7d15b4
SurendharSSenthil <surendharsenthil257@gmail.com>
2024-09-01 23:01:21 +0530

    update for contact

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,12 +5,12 @@ import contact from "/Contact.png";
 <footer
     class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center relative"
 >
-    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium mb-4">
         Connect with me &darr;
     </p>
-    <div class="flex flex-col sm:flex-row justify-between items-center w-full">
+    <div class="flex flex-col sm:flex-row justify-between items-center sm:gap-4 gap-12 w-full">
      <div class="flex justify-center items-center sm:w-1/2">
-            <img src={"/Contact.png"} alt="contact" class="max-h-80 w-auto object-contain" />
+            <img src={"/Contact.png"} alt="contact" class="max-h-80 w-auto object-contain hover:shadow-[0_0_10px_rgba(138,43,226,0.8)]" />
         </div>
         <div class="flex flex-col gap-4 items-center justify-center sm:w-1/2 text-center sm:text-left">
             <p>
